f7ed96d61ffdd8dfd03e59de9df002921a42e222
qjari calizaya <qjarishekka@outlook.com>
2025-08-07 08:39:21 +0300

    pagina inicio

---
+++ models/Product.js
@@ -0,0 +1,13 @@
+﻿import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  descripcion: { type: String },
+  fabricante: {type:String},
+  precio: { type: Number, required: true },
+  imagen: { type: String },
+  categoria: { type: String },
+  stock: { type: Number, default: 0 },
+}, { timestamps: true });
+
+export default mongoose.models.Product || mongoose.model('Product', ProductSchema);
---
+++ pages/api/productos.js
@@ -0,0 +1,28 @@
+﻿// pages/api/productos.js
+
+import connectDB from '@/lib/mongodb';
+import Product from '@/models/Product';
+
+export default async function handler(req, res) {
+  await connectDB();
+
+  if (req.method === 'POST') {
+    try {
+      const nuevoProducto = await Product.create(req.body);
+      return res.status(201).json(nuevoProducto);
+    } catch (error) {
+      return res.status(400).json({ error: 'Error al crear el producto' });
+    }
+  }
+
+  if (req.method === 'GET') {
+    try {
+      const productos = await Product.find();
+      return res.status(200).json(productos);
+    } catch (error) {
+      return res.status(500).json({ error: 'Error al obtener productos' });
+    }
+  }
+
+  return res.status(405).json({ error: 'Método no permitido' });
+}
